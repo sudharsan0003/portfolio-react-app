8c4258350df0d5661dfea6a56a19b9edffcab004
sudharsan0003 <sanmarly04@gmail.com>
2023-12-04 13:38:24 +0530

    projects

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,25 +3,49 @@ import { reactData } from '../data/reactData';
 import { FaExternalLinkAlt } from 'react-icons/fa';
 import { FaGithub } from 'react-icons/fa6';
 import { jsData } from '../data/jsData';
-import Slider from 'react-slider';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
 
 const Work = () => {
-  // projects file
   const project = reactData;
   const projects = jsData;
-  //setProject(data);
+
+  const options = {
+    loop: false,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    slideSpeed: 200,
+    pullDrag: true,
+    dots: false,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 2,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 4,
+      },
+    },
+  };
 
   return (
-    <div name='work' className='w-full h-screen  text-gray-300 bg-[#0a192f]'>
+    <div name='work' className='w-full h-max  text-gray-300 bg-[#0a192f]'>
       <div className='text-center bg-[#0a192f] pt-32 '>
         <p className='text-4xl font-bold inline border-b-4 border-orange-300   '>
           Works
         </p>
         <p className='py-6'>// Check out some of my recent work</p>
       </div>
-      <div className='max-w-[650px] mx-auto my-5 flex flex-col justify-center w-full h-full'>
+      <div className='max-w-[650px] mx-auto my-5 flex flex-col justify-center w-full h-full bg-[#0a192f]'>
         {project.map((item) => (
           <div
             className='shadow-md h-2/4 shadow-orange-300 group container rounded-md
@@ -40,7 +64,7 @@ const Work = () => {
               <a href={item.live} target='_blank'>
                 <button
                   className='text-center rounded-lg px-2 py-2 
-                  bg-white text-gray-700  '
+                  bg-white text-gray-700 duration-200 hover:scale-150 p-3 hover:bg-transparent '
                 >
                   <FaExternalLinkAlt />
                 </button>
@@ -48,7 +72,7 @@ const Work = () => {
               <a href={item.github} target='_blank'>
                 <button
                   className='text-center rounded-lg px-2 py-2 
-                  bg-white text-gray-700'
+                  bg-white text-gray-700 duration-200 hover:scale-150 p-3 hover:bg-transparent'
                 >
                   <FaGithub />
                 </button>
@@ -57,46 +81,43 @@ const Work = () => {
           </div>
         ))}
       </div>
+      <div className='w-3/4 mx-auto my-16  '>
+        <OwlCarousel
+          className='owl-theme w-full
+          '
+          {...options}
+        >
+          {projects.map(({ id, name, image, github, live }) => (
+            <div className='w-full border  shadow-orange-300 shadow-md rounded-md'>
+              <img
+                src={image}
+                alt='project'
+                className='rounded-md w-3/4 duration-200 hover:scale-105 p-3'
+              />
+              <div className='flex items-center justify-between p-3 bg-[#0a192f]'>
+                <a href={live} target='_blank'>
+                  <button
+                    className='text-center rounded-lg px-2 py-2 
+                  bg-white text-gray-700 duration-200 hover:scale-150 p-3 hover:bg-transparent'
+                  >
+                    <FaExternalLinkAlt size={10} />
+                  </button>
+                </a>
+                <a href={github} target='_blank'>
+                  <button
+                    className='text-center rounded-lg px-2 py-2 
+                  bg-white text-gray-700 duration-200 hover:scale-150 p-3 hover:bg-transparent'
+                  >
+                    <FaGithub size={10} />
+                  </button>
+                </a>
+              </div>
+            </div>
+          ))}
+        </OwlCarousel>
+      </div>
     </div>
   );
 };
 
 export default Work;
-
-// {
-//   project.map((item) => (
-//     <div
-//       style={{
-//         backgroundImage: `url(${item.image})`,
-//         backgroundRepeat: 'no-repeat',
-//         backgroundSize: 'cover',
-//       }}
-//       className='shadow-md  shadow-orange-300 group container rounded-md
-//               flex justify-center text-center items-center mx-auto mt-3  '
-//     >
-//       <div className=' '>
-//         <span className='text-2xl font bold text-white tracking-wider '>
-//           {item.name}
-//         </span>
-// <div className='pt-8 text-center '>
-//   <a href={item.live} target='_blank'>
-//     <button
-//       className='text-center rounded-lg px-4 py-3 m-2
-//                bg-white text-gray-700 font-bold text-lg'
-//     >
-//       Live
-//     </button>
-//   </a>
-//   <a href={item.github} target='_blank'>
-//     <button
-//       className='text-center rounded-lg px-4 py-3 m-2
-//                bg-white text-gray-700 font-bold text-lg'
-//     >
-//       Code
-//     </button>
-//   </a>
-// </div>
-//       </div>
-//     </div>
-//   ));
-// }
