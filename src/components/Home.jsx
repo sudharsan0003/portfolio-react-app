3648790055fcaf43914458f4852c06e1d4456c85
sudharsan0003 <sanmarly04@gmail.com>
2023-12-12 23:10:54 +0530

    final

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -29,9 +29,9 @@ const Home = () => {
           />
         </h1>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
-          I’m a full-stack developer specializing in building (and occasionally
-          designing) exceptional digital experiences. Currently, I’m focused on
-          building responsive full-stack web applications.
+          I'm a full-stack developer specializing in the creation of immersive
+          digital experiences. My passion for both development. Currently
+          immersed in the world of responsive full-stack web applications.
         </p>
         <div className='flex mt-8 gap-2'>
           <div className='flex items-center justify-center'>
